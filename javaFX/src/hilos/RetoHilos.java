bc6a1329e0ac49dedb52a86c875bc17bce966ad9
JavierVelaR <javelo999@gmail.com>
2024-02-19 23:24:10 +0100

    Actualizar reto hilos

--- javaFX/src/hilos/RetoHilos.java
+++ javaFX/src/hilos/RetoHilos.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,18 +13,37 @@ public class RetoHilos extends Application{
 
 	@Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("CalculadoraUltima.fxml"));
+		Label label = new Label("Soy el reto de los hilos");
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
 
-        // Crear y empezar un hilo simple
-        Thread thread = new Thread(() -> { //crea un hilo
+        generarHilo("Por favor", label, 1000);
+        generarHilo("Deja de ejecutarme", label, 2000);
+        generarHilo("No ves que funciono", label, 3000);
+        generarHilo("Ponme un 10", label, 4000);
+        generarHilo("Gracias alberto :)", label, 5000);
+        
+        Scene scene = new Scene(root, 300, 250); //crea una nueva escena
+
+        String cssFile = getClass().getResource("style.css").toExternalForm();
+        
+        scene.getStylesheets().add(cssFile);
+        
+        primaryStage.setTitle("Reto hilos JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+	
+	public void generarHilo(String msg, Label label, int espera) {
+		Thread thread = new Thread(() -> { //crea un hilo
             try {
-                // Dormir el hilo por 3 segundos
-                Thread.sleep(3000);
+                // Dormir el hilo por 1 segundo
+                Thread.sleep(espera);
 
 
                 // Después de 3 segundos, actualizar la interfaz gráfica
                 Platform.runLater(() -> {
-                    
+                    label.setText(msg);
                 });
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -34,14 +51,7 @@ public class RetoHilos extends Application{
         });
         thread.start(); //inicia el hilo creado anteriormente
 
-
-        Scene scene = new Scene(root, 300, 250); //crea una nueva escena
-
-
-        primaryStage.setTitle("JavaFX Thread Example");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+	}
 	
 	public static void main(String[] args) {
 		launch(args);
